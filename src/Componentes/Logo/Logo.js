dae8875aa08cede74ad2df86c9586620008921a3
Felype Paiva <dev.felype@gmail.com>
2025-05-16 08:11:40 -0300

    Centraliza texto da logo verticalmente

--- src/Componentes/Logo/Logo.js
+++ src/Componentes/Logo/Logo.js
@@ -1,10 +1,11 @@
 import ImgLogo from '../../imagens/logo.svg'
+import './Logo.css'
 
 function Logo(){
     return(
-        <div>
+        <div className='logo'>
             <img src={ImgLogo} alt="Imagem logo de Delícias à Mesa"></img>
-            <span>Delícias à Mesa</span>
+            <span className='logoTxt'>Delícias à Mesa</span>
         </div>
     );
 }
